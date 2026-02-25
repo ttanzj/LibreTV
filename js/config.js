d76b719a8709a85747c20e6a96355a26bb1a4486
ttanzj <155122389+ttanzj@users.noreply.github.com>
2026-02-25 10:50:11 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -114,6 +114,11 @@ const API_SITES = {
         api: 'https://cj.lziapi.com/api.php/provide/vod/',
         name: '量子资源站'
     },
+    ckzy: {
+        api: 'https://www.ckzy1.com',
+        name: 'CK资源',
+        adult: true
+    },
     testSource: {
         api: 'https://www.example.com/api.php/provide/vod',
         name: '空内容测试源',
