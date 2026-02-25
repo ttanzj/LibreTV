6ca641f839d19080fc24fc6f2336788ef8e06e26
ttanzj <155122389+ttanzj@users.noreply.github.com>
2026-02-25 14:18:26 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -84,8 +84,8 @@ name: '魔爪资源',
 },
 mdzy: {
 api: 'https://www.mdzyapi.com/api.php/provide/vod',
-name: '魔都资源',
-},
+name: '魔都资源'，
+}，
 zuid: {
 api: 'https://api.zuidapi.com/api.php/provide/vod',
 name: '最大资源'
@@ -114,11 +114,47 @@ lzi: {
 api: 'https://cj.lziapi.com/api.php/provide/vod/',
 name: '量子资源站'
 },
-    ckzy: {
-        api: 'https://www.ckzy1.com',
-        name: 'CK资源',
-        adult: true
-    },
+ckzy: {
+api: 'https://www.ckzy1.com',
+name: 'CK资源',
+adult: true
+},
+jkun: {
+api: 'https://jkunzyapi.com',
+name: 'jkun资源',
+adult: true
+},
+bwzy: {
+api: 'https://api.bwzym3u8.com',
+name: '百万资源',
+adult: true
+},
+souav: {
+api: 'https://api.souavzy.vip',
+name: 'souav资源',
+adult: true
+}，
+r155: {
+api: 'https://155api.com',
+name: '155资源',
+adult: true
+},
+lsb: {
+api: 'https://apilsbzy1.com',
+name: 'lsb资源',
+adult: true
+},
+huangcang: {
+api: 'https://hsckzy.vip'，
+name: '黄色仓库',
+adult: true,
+detail: 'https://hsckzy.vip'
+},
+yutu: {
+api: 'https://yutuzy10.com',
+name: '玉兔资源',
+adult: true
+},
 testSource: {
 api: 'https://www.example.com/api.php/provide/vod',
 name: '空内容测试源',
